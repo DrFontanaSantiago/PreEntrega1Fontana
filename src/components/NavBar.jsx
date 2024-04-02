c55964ac86840f1d7dc4c8f3cf9bac234426da24
DrFontanaSantiago <sfontanaestudio@gmail.com>
2024-04-02 12:34:46 -0300

    finalizando tercer entrega

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-bootstrap";
 
 import { CartWidget } from "./CartWidget";
 
@@ -8,11 +9,17 @@ export const NavBar = () => {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Área 51 Shop</Navbar.Brand>
+        <Navbar.Brand to="#home">Área 51 Shop</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Torso</Nav.Link>
-          <Nav.Link href="#features">Bermudas</Nav.Link>
-          <Nav.Link href="#pricing">Accesorios</Nav.Link>
+          <Nav.Link to="/category/torso" as={NavLink}>
+            Torso
+          </Nav.Link>
+          <Nav.Link to="/category/bermudas" as={NavLink}>
+            Bermudas
+          </Nav.Link>
+          <Nav.Link to="/category/accesorios" as={NavLink}>
+            Accesorios
+          </Nav.Link>
         </Nav>
         <CartWidget />
       </Container>
